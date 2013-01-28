bd1c0fe3d5338dce93b1e5a56f24bcbf07ef2037
nuxlic <cangini.f@hotmail.com>
2013-01-27 21:21:22 -0300

    Codigo de ejemplo de uso en el main

--- src/ar/com/vioflaInc/main/Main.java
+++ src/ar/com/vioflaInc/main/Main.java
@@ -2,11 +2,12 @@ package ar.com.vioflaInc.main;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-//		Script sc = new Script();
+//		Script sc = new Script("/home/viofla/Escritorio/MiScript.sh");
 //		sc.setVerbose(true);
-//		String CONTENT = "sqlplus2\nls -l\nps\necho \"End\\nFin\"";
-//		String OUTPUT = sc.executeScript(CONTENT,
-//		"/home/viofla/Escritorio/MiScript.sh", true);
+//		//String CONTENT = "sqlplus2\nls -l\nps\necho \"End\\nFin\"";
+////		String OUTPUT = sc.executeScript(CONTENT,
+////				"/home/viofla/Escritorio/MiScript.sh", true);
+//		String OUTPUT = sc.executeCommand();
 //		System.out.println(OUTPUT);
 //		String ERROR = sc.getError();
 //		System.out.println("**ERROR**" + ERROR);
